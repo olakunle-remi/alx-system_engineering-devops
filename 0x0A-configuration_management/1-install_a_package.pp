42b98b42a735a469ca2333b71ed4bf7153b78038
olakunle-remi <abbeypentious@gmail.com>
2024-03-23 13:15:21 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Executes a bash command
+exec { 'killmenow':
+  command    => '/usr/bin/pkill killmenow',
+  provider =>'shell',
+  returns =>[0, 1],
 }
