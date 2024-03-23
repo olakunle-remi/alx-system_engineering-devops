88f05a14405a62ba7cc26bd8ecf0a0fa5c110959
olakunle-remi <abbeypentious@gmail.com>
2024-03-23 13:33:03 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-##!/usr/bin/pup
-# Using Puppt to install flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
