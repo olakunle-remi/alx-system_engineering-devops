34312e6648a011aa48c65d43b433bb2e60ea3da0
olakunle-remi <91887981+olakunle-remi@users.noreply.github.com>
2024-03-26 00:37:27 +0100

    Update 1-install_a_package.pp
    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-from werkzeug.urls import url_quote
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+#!/usr/bin/pup
+#Install puppet-lint
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip'
 }
