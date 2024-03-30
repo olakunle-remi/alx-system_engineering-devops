e73afcb7b37dd75de77e54923adccfebcb813d56
olakunle-remi <abbeypentious@gmail.com>
2024-03-30 05:50:40 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Puppet script that install flask
-
-file { 'flask':
+  package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
